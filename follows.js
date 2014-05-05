fac7d65ea5dc7c11676e7cbaca6d45cd31bff674
JitoChile <JitoChile@users.noreply.github.com>
2014-05-05 11:23:36 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -13,7 +13,7 @@
 	*/
 
 	var defaultKey		= 'kvXeeW8IvIKaWPiInmAdR3UFgKlSrCVGO6mx90mV72OeuFJF', // Unique master Xively API key to be used as a default
-		defaultFeeds	= ['1872553213'], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= '1872553213', // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Universidad de los Andes', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
